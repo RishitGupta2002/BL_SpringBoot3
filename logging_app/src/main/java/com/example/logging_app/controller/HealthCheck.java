759d7a593eca99ec21fe95b0385be51c25407add
Rishit Gupta <RishitGupta786@gmail.com>
2025-03-02 17:27:58 +0530

    Added Logging Function in Health file

---
+++ logging_app/src/main/java/com/example/logging_app/controller/HealthCheck.java
@@ -0,0 +1,21 @@
+package com.example.logging_app.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheck.class);
+    @GetMapping("/")
+    public String healthCheck() {
+        logger.trace("TRACE: Health check endpoint called");
+        logger.debug("DEBUG: Health check endpoint processing");
+        logger.info("INFO: Health check is OK");
+        logger.warn("WARN: Health check warning (example)");
+        logger.error("ERROR: Health check error (example)");
+        return "Working";
+    }
+}
